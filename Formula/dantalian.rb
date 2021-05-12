35c630aa69a6c11e9c8fb857224336ecc7cbc7f9
gyara <me@gyara.moe>
2021-05-12 14:59:31 +0900

    dantalian 0.3.0

--- Formula/dantalian.rb
+++ Formula/dantalian.rb
@@ -1,9 +1,9 @@
 class Dantalian < Formula
-  desc "A nfo file generator for your anime. Source from Bangumi."
+  desc "Nfo file generator for your anime, Source from Bangumi"
   homepage "https://github.com/nanozuki/dantalian"
   url "https://github.com/nanozuki/dantalian/archive/refs/tags/v0.2.1.tar.gz"
-  sha256 "e92b125e498ca2e72a705c23ada608be7f66ecfb334aa3715087516f411c049b"
-  license "GPL3"
+  sha256 "b2f4d0ca2e57a94b7b96053a7e7751571621d21acc233f31ed8ea193f88be6c3"
+  license "GPL-3.0-only"
   head "https://github.com/nanozuki/dantalian"
 
   depends_on "rust" => :build
@@ -11,5 +11,8 @@ class Dantalian < Formula
   def install
     ENV["SHELL_COMPLETIONS_DIR"] = buildpath
     system "cargo", "install", *std_cargo_args
+    bash_completion.install "dantalian.bash"
+    fish_completion.install "dantalian.fish"
+    zsh_completion.install "contrib/completion/_dantalian"
   end
 end
