405e0176ec2c9e72b34b87c290d4a891f2802aeb
gyara <me@gyara.moe>
2021-05-13 12:49:07 +0900

    update

--- Formula/dantalian.rb
+++ Formula/dantalian.rb
@@ -1,7 +1,7 @@
 class Dantalian < Formula
   desc "Nfo file generator for your anime, Source from Bangumi"
   homepage "https://github.com/nanozuki/dantalian"
-  url "https://github.com/nanozuki/dantalian/archive/refs/tags/v0.2.1.tar.gz"
+  url "https://github.com/nanozuki/dantalian/archive/refs/tags/v0.3.0.tar.gz"
   sha256 "b2f4d0ca2e57a94b7b96053a7e7751571621d21acc233f31ed8ea193f88be6c3"
   license "GPL-3.0-only"
   head "https://github.com/nanozuki/dantalian"
